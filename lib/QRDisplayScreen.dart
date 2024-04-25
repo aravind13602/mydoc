8d73ca26aff82ead2d90e9549aa44915afae3b16
aravind13602 <153479464+aravind13602@users.noreply.github.com>
2024-04-25 13:40:45 +0530

    monog db

--- lib/QRDisplayScreen.dart
+++ lib/QRDisplayScreen.dart
@@ -1,3 +1,69 @@
+// import 'dart:typed_data';
+// import 'package:flutter/material.dart';
+// import 'package:qr_flutter/qr_flutter.dart';
+// import 'package:image_gallery_saver/image_gallery_saver.dart';
+//
+// class QRDisplayScreen extends StatelessWidget {
+//   final String qrData;
+//
+//   QRDisplayScreen({required this.qrData});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('Generated QR Code'),
+//         actions: [
+//           IconButton(
+//             icon: Icon(Icons.save),
+//             onPressed: () => _saveQrImage(context),
+//           ),
+//         ],
+//       ),
+//       body: Center(
+//         child: FutureBuilder<Uint8List?>(
+//           future: _generateQrImageData(),
+//           builder: (context, snapshot) {
+//             if (snapshot.connectionState == ConnectionState.waiting) {
+//               return CircularProgressIndicator();
+//             } else if (snapshot.hasError) {
+//               return Text('Error: ${snapshot.error}');
+//             } else if (snapshot.hasData) {
+//               return Image.memory(snapshot.data!);
+//             } else {
+//               return Text('No data');
+//             }
+//           },
+//         ),
+//       ),
+//     );
+//   }
+//
+//   Future<Uint8List?> _generateQrImageData() async {
+//     ByteData? byteData = await QrPainter(
+//       data: qrData,
+//       version: QrVersions.auto,
+//     ).toImageData(200);
+//     if (byteData != null) {
+//       return byteData.buffer.asUint8List();
+//     }
+//     return null;
+//   }
+//
+//   Future<void> _saveQrImage(BuildContext context) async {
+//     final Uint8List? qrImageData = await _generateQrImageData();
+//     if (qrImageData != null) {
+//       await ImageGallerySaver.saveImage(qrImageData);
+//       ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+//         content: Text('QR code image saved to gallery'),
+//       ));
+//     } else {
+//       ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+//         content: Text('Failed to save QR code image'),
+//       ));
+//     }
+//   }
+// }
 import 'dart:typed_data';
 import 'package:flutter/material.dart';
 import 'package:qr_flutter/qr_flutter.dart';
@@ -20,7 +86,7 @@ class QRDisplayScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: Center(
+      body: MyCenterWidget(  // Use MyCenterWidget here
         child: FutureBuilder<Uint8List?>(
           future: _generateQrImageData(),
           builder: (context, snapshot) {
@@ -62,5 +128,19 @@ class QRDisplayScreen extends StatelessWidget {
         content: Text('Failed to save QR code image'),
       ));
     }
+  }
+}
+
+// Define MyCenterWidget
+class MyCenterWidget extends StatelessWidget {
+  final Widget child;
+
+  MyCenterWidget({required this.child});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: child,
+    );
   }
 }
--- lib/new_request.dart
+++ lib/new_request.dart
@@ -1,6 +1,79 @@
+// import 'package:flutter/material.dart';
+// import 'package:mydoc/QRDisplayScreen.dart';
+//
+//
+// class NewRequestScreen extends StatelessWidget {
+//   final String userName;
+//
+//   NewRequestScreen({required this.userName});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     TextEditingController toController = TextEditingController();
+//     TextEditingController documentNumberController = TextEditingController();
+//     TextEditingController subController = TextEditingController();
+//
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('New Request'),
+//       ),
+//       body: Padding(
+//         padding: EdgeInsets.all(16.0),
+//         child: Column(
+//           crossAxisAlignment: CrossAxisAlignment.stretch,
+//           children: [
+//             Text(
+//               'From: $userName',
+//               style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+//             ),
+//             SizedBox(height: 16.0),
+//             TextFormField(
+//               controller: toController,
+//               decoration: InputDecoration(labelText: 'To'),
+//             ),
+//             SizedBox(height: 16.0),
+//             TextFormField(
+//               controller: documentNumberController,
+//               decoration: InputDecoration(labelText: 'Document Number'),
+//             ),
+//             SizedBox(height: 16.0),
+//             TextFormField(
+//               controller: subController,
+//               decoration: InputDecoration(labelText: 'Sub:'),
+//             ),
+//             SizedBox(height: 32.0),
+//             ElevatedButton(
+//               onPressed: () {
+//                 String documentNumber = documentNumberController.text;
+//                 String sub = subController.text;
+//                 String to = toController.text;
+//
+//                 // Generate QR code data
+//                 String qrData = 'Document Number: $documentNumber';
+//
+//                 // Navigate to QRDisplayScreen and pass qrData
+//                 Navigator.push(
+//                   context,
+//                   MaterialPageRoute(
+//                     builder: (context) => QRDisplayScreen(qrData: qrData),
+//                   ),
+//                 );
+//               },
+//               style: ElevatedButton.styleFrom(
+//                 backgroundColor: Color.fromRGBO(230, 18, 14, 1), // Set button color
+//               ),
+//               child: Text('Generate QR', style: TextStyle(color: Colors.white)),
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:mongo_dart/mongo_dart.dart';
 import 'package:mydoc/QRDisplayScreen.dart';
-
 
 class NewRequestScreen extends StatelessWidget {
   final String userName;
@@ -43,13 +116,38 @@ class NewRequestScreen extends StatelessWidget {
             ),
             SizedBox(height: 32.0),
             ElevatedButton(
-              onPressed: () {
+              onPressed: () async {
                 String documentNumber = documentNumberController.text;
                 String sub = subController.text;
                 String to = toController.text;
 
+                print('Document Number: $documentNumber');
+
+                // Fetch user details from Firebase
+                User? user = FirebaseAuth.instance.currentUser;
+                String? firebaseId = user?.uid;
+
+                print('Firebase ID: $firebaseId');
+
+                // Save data to MongoDB
+                var db = Db('mongodb+srv://aravindbs001:9elkwiUCxLuYeDsD@cluster0.juuxnxc.mongodb.net/mydoc?retryWrites=true&w=majority&appName=Cluster0');
+                await db.open();
+
+                var collection = db.collection('userdetails');
+                await collection.insert({
+                  'from': userName,
+                  'to': to,
+                  'documentNumber': documentNumber,
+                  'sub': sub,
+                  'firebaseId': firebaseId,
+                });
+
+                await db.close();
+
                 // Generate QR code data
                 String qrData = 'Document Number: $documentNumber';
+
+                print('QR Data: $qrData');
 
                 // Navigate to QRDisplayScreen and pass qrData
                 Navigator.push(
@@ -57,7 +155,11 @@ class NewRequestScreen extends StatelessWidget {
                   MaterialPageRoute(
                     builder: (context) => QRDisplayScreen(qrData: qrData),
                   ),
-                );
+                ).then((value) {
+                  print('Navigation completed');
+                }).catchError((error) {
+                  print('Navigation error: $error');
+                });
               },
               style: ElevatedButton.styleFrom(
                 backgroundColor: Color.fromRGBO(230, 18, 14, 1), // Set button color
@@ -70,125 +172,5 @@ class NewRequestScreen extends StatelessWidget {
     );
   }
 }
-// import 'package:flutter/material.dart';
-// import 'package:mongo_dart/mongo_dart.dart';
-// import 'package:mydoc/QRDisplayScreen.dart';
-// import 'package:firebase_auth/firebase_auth.dart';
-//
-// class NewRequestScreen extends StatefulWidget {
-//   final String userName;
-//
-//   NewRequestScreen({required this.userName});
-//
-//   @override
-//   _NewRequestScreenState createState() => _NewRequestScreenState();
-// }
-//
-// class _NewRequestScreenState extends State<NewRequestScreen> {
-//   final TextEditingController toController = TextEditingController();
-//   final TextEditingController documentNumberController = TextEditingController();
-//   final TextEditingController subController = TextEditingController();
-//   final FirebaseAuth _auth = FirebaseAuth.instance;
-//
-//   late Db db;
-//
-//   @override
-//   void initState() {
-//     super.initState();
-//     _connectToDb();
-//   }
-//
-//   _connectToDb() async {
-//     db = Db('mongodb+srv://aravindbs001:<XMexpuSLDQ6Tk9Ym>@cluster0.juuxnxc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
-//     try {
-//       await db.open();
-//       print('Connected to MongoDB');
-//     } catch (e) {
-//       print('Error connecting to MongoDB: $e');
-//     }
-//   }
-//
-//   @override
-//   void dispose() {
-//     db.close();
-//     super.dispose();
-//   }
-//
-//   Future<void> _insertData(String documentNumber, String to, String sub) async {
-//     await db.open();
-//
-//     try {
-//       await db.collection('userdetails').insert({
-//         'documentNumber': documentNumber,
-//         'from': widget.userName,
-//         'to': to,
-//         'sub': sub,
-//       });
-//       print('Data inserted successfully into userdetails');
-//     } catch (e) {
-//       print('Error inserting data into userdetails: $e');
-//     }
-//   }
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text('New Request'),
-//       ),
-//       body: Padding(
-//         padding: EdgeInsets.all(16.0),
-//         child: Column(
-//           crossAxisAlignment: CrossAxisAlignment.stretch,
-//           children: [
-//             Text(
-//               'From: ${widget.userName}',
-//               style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
-//             ),
-//             SizedBox(height: 16.0),
-//             TextFormField(
-//               controller: toController,
-//               decoration: InputDecoration(labelText: 'To'),
-//             ),
-//             SizedBox(height: 16.0),
-//             TextFormField(
-//               controller: documentNumberController,
-//               decoration: InputDecoration(labelText: 'Document Number'),
-//             ),
-//             SizedBox(height: 16.0),
-//             TextFormField(
-//               controller: subController,
-//               decoration: InputDecoration(labelText: 'Sub:'),
-//             ),
-//             SizedBox(height: 32.0),
-//             ElevatedButton(
-//               onPressed: () async {
-//                 String documentNumber = documentNumberController.text;
-//                 String sub = subController.text;
-//                 String to = toController.text;
-//
-//                 // Insert data into MongoDB
-//                 await _insertData(documentNumber, to, sub);
-//
-//                 // Generate QR code data
-//                 String qrData = 'Document Number: $documentNumber';
-//
-//                 // Navigate to QRDisplayScreen and pass qrData
-//                 Navigator.push(
-//                   context,
-//                   MaterialPageRoute(
-//                     builder: (context) => QRDisplayScreen(qrData: qrData),
-//                   ),
-//                 );
-//               },
-//               style: ElevatedButton.styleFrom(
-//                 backgroundColor: Color.fromRGBO(230, 18, 14, 1),
-//               ),
-//               child: Text('Generate QR', style: TextStyle(color: Colors.white)),
-//             ),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
+
+
