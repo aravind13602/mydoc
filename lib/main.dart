c1a5ea1746e6aaba5bc67c971fdadf2cdd66525a
aravind13602 <153479464+aravind13602@users.noreply.github.com>
2024-05-28 12:37:46 +0530

    done & dusted

--- lib/main.dart
+++ lib/main.dart
@@ -27,32 +27,3 @@ class MyApp extends StatelessWidget {
 }
 
 
-// import 'package:flutter/material.dart';
-// import 'package:firebase_core/firebase_core.dart';
-// import 'package:mydoc/login_page.dart';
-// import 'firebase_options.dart'; // Make sure to import your firebase_options.dart
-//
-// void main() async {
-//   WidgetsFlutterBinding.ensureInitialized(); // Ensure Flutter bindings are initialized
-//   await Firebase.initializeApp(
-//     options: DefaultFirebaseOptions.currentPlatform,
-//   );
-//   runApp(MyApp());
-// }
-//
-// class MyApp extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'MyDoc',
-//       debugShowCheckedModeBanner: false, // Remove the debug banner
-//
-//
-//
-//       theme: ThemeData(
-//         primarySwatch: Colors.blue,
-//       ),
-//       home: LoginPage(), // Assuming LoginPage is your login page
-//     );
-//   }
-// }
